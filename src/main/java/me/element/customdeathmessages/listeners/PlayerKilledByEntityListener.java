4167c6e252bdfd16ddf414961864e1a732815db7
Legoman99573 <7460704+Legoman99573@users.noreply.github.com>
2021-09-06 03:22:37 -0500

    Moved to switch statement in PlayerKilledByEntityListener.java This makes it look alot nicer and removes some unneeded code as entity is converted to string. Making the enum for EntityType and Server Version check redundant mostly except for arrow messages ofc.
    Signed-off-by: Legoman99573 <7460704+Legoman99573@users.noreply.github.com>

--- src/main/java/me/element/customdeathmessages/listeners/PlayerKilledByEntityListener.java
+++ src/main/java/me/element/customdeathmessages/listeners/PlayerKilledByEntityListener.java
@@ -39,118 +39,211 @@ public class PlayerKilledByEntityListener implements Listener {
 
 					boolean hasCustomName = event.getDamager().getCustomName() != null;
 
-					if (entity == EntityType.SLIME) {
-						path = "slime-messages";
-					} else if (entity == EntityType.SKELETON) { // added before supported v
-						path = "skeleton-messages";   		
-					} else if (entity == EntityType.MAGMA_CUBE) { // added before supported v
-						path = "magmacube-messages";
-					} else if (entity == EntityType.SPIDER) { // added before supported v
-						path = "spider-messages";
-					} else if (entity == EntityType.CAVE_SPIDER) { // added before supported v
-						path = "cavespider-messages";
-					} else if (entity == EntityType.WITHER) { // added before supported v
-						path = "witherboss-messages";
-					} else if (entity == EntityType.ENDER_DRAGON) { // added before supported v
-						path = "dragon-messages";
-					} else if (entity == EntityType.PRIMED_TNT) { // added before supported v
-						path = "tnt-messages";
-					} else if (entity == EntityType.CREEPER) { // added before supported v
-						path = "creeper-messages";
-					} else if (entity == EntityType.GHAST) { // added before supported v
-						path = "ghast-messages";
-					} else if (entity == EntityType.ENDERMAN) { // added before supported v
-						path = "enderman-messages";
-					} else if (entity == EntityType.SILVERFISH) { // added before supported v
-						path = "silverfish-messages";
-					} else if (entity == EntityType.WITCH) { // added before supported v
-						path = "witch-messages";
-					} else if (entity == EntityType.GUARDIAN) { // added before supported v
-						path = "guardian-messages";
-					} else if (entity == EntityType.IRON_GOLEM) { // added before supported v
-						path = "golem-messages";
-					} else if (entity == EntityType.ENDERMITE) { // added before supported v
-						path = "endermite-messages";
-					} else if (entity == EntityType.ZOMBIE) { // added before supported v
-						path = "zombie-messages";
-					} else if (entity == EntityType.WOLF) { // added before supported v
-						path = "wolf-messages";
-					} else if (entity == EntityType.BLAZE) { // added before supported v
-						path = "blaze-messages";
-					} else if (entity == EntityType.FIREBALL) { // added before supported v
-						path = "fireball-messages";
-					} else if (entity == EntityType.ARROW) // added before supported v
-					{ 
-						if (((Projectile) event.getDamager()).getShooter() instanceof Skeleton) {
+					switch(entity.toString()) {
+						case "SLIME":
+							path = "slime-messages";
+							break;
+
+						case "SKELETON":
 							path = "skeleton-messages";
-						} else if (versionInt >= VersionEnums.VERSION_110.getVersionInt() && ((Projectile) event.getDamager()).getShooter() instanceof Stray) {
+							break;
+
+						case "MAGMA_CUBE":
+							path = "magmacube-messages";
+							break;
+
+						case "SPIDER":
+							path = "spider-messages";
+							break;
+
+						case "CAVE_SPIDER":
+							path = "cavespider-messages";
+							break;
+
+						case "WITHER":
+							path = "witherboss-messages";
+							break;
+
+						case "DRAGON_FIREBALL": // Since 1.11
+						case "ENDER_DRAGON":
+							path = "dragon-messages";
+							break;
+
+						case "PRIMED_TNT":
+							path = "tnt-messages";
+							break;
+
+						case "CREEPER":
+							path = "creeper-messages";
+							break;
+
+						case "GHAST":
+							path = "ghast-messages";
+							break;
+
+						case "ENDERMAN":
+							path = "enderman-messages";
+							break;
+
+						case "SILVERFISH":
+							path = "silverfish-messages";
+							break;
+
+						case "WITCH":
+							path = "witch-messages";
+							break;
+
+						case "GUARDIAN":
+							path = "guardian-messages";
+							break;
+
+						case "IRON_GOLEM":
+							path = "golem-messages";
+							break;
+
+						case "ENDERMITE":
+							path = "endermite-messages";
+							break;
+
+						case "ZOMBIE":
+							path = "zombie-messages";
+							break;
+
+						case "WOLF":
+							path = "wolf-messages";
+							break;
+
+						case "BLAZE":
+							path = "blaze-messages";
+							break;
+
+						case "FIREBALL":
+							path = "fireball-messages";
+							break;
+
+						case "ARROW":
+							if (((Projectile) event.getDamager()).getShooter() instanceof Skeleton) {
+								path = "skeleton-messages";
+							} else if (versionInt >= VersionEnums.VERSION_110.getVersionInt() && ((Projectile) event.getDamager()).getShooter() instanceof Stray) {
+								path = "stray-messages";
+							} else if (versionInt >= VersionEnums.VERSION_114.getVersionInt() && ((Projectile) event.getDamager()).getShooter() instanceof Pillager) {
+								path = "pillager-messages";
+							} else {
+								path = "arrow-messages";
+							}
+							break;
+						case "PIG_ZOMBIE": // Removed in 1.15 without an alternative. Left here for backwards compatablilty for legacy versions below 1.15. Completely removed in 1.16 in favor of zombie piglins
+							path = "pigman-messages";
+							break;
+
+						case "SHULKER": // Since 1.9
+							path = "shulker-messages";
+							break;
+
+						case "STRAY": // Since 1.10
 							path = "stray-messages";
-						} else if (versionInt >= VersionEnums.VERSION_114.getVersionInt() && ((Projectile) event.getDamager()).getShooter() instanceof Pillager) {
+							break;
+
+						case "LLAMA_SPIT": // Since 1.11
+						case "LLAMA": // Since 1.11
+						case "TRADER_LLAMA": //Since 1.14
+							path = "llama-messages";
+							break;
+
+						case "WITHER_SKELETON": // Since 1.11 - differation of wither skeletons and skeletons
+							path = "lightning-messages";
+							break;
+
+						case "ELDER_GUARDIAN": // Since 1.11 - differation of elder guardians and guardians
+							path = "elderguardian-messages";
+							break;
+
+						case "ZOMBIE_VILLAGER": // Since 1.11 - differation of zombies and zombie villagers
+							path = "zombievillager-messages";
+							break;
+
+						case "VEX": // Since 1.11
+							path = "vex-messages";
+							break;
+
+						case "VINDICATOR": // Since 1.11
+							path = "vindicator-messages";
+							break;
+
+						case "HUSK": // Since 1.11
+							path = "husk-messages";
+							break;
+
+						case "EVOKER": // Since 1.11
+							path = "evoker-messages";
+							break;
+
+						case "ILLUSIONER": // Since 1.12
+							path = "illusioner-messages";
+							break;
+
+						case "PHANTOM": // Since 1.13
+							path = "phantom-messages";
+							break;
+
+						case "DROWNED": // Since 1.13
+							path = "drowned-messages";
+							break;
+
+						case "PUFFERFISH": // Since 1.13
+							path = "pufferfish-messages";
+							break;
+
+						case "POLAR_BEAR": // Since 1.13
+							path = "polar-bear-messages";
+							break;
+
+						case "DOLPHIN": // Since 1.13
+							path = "dolphin-messages";
+							break;
+
+						case "PILLAGER": // Since 1.14
 							path = "pillager-messages";
-						} else {
-							path = "arrow-messages";
-						}
-					} else if (entity == EntityType.LIGHTNING) { // 1.11
-						path = "lightning-messages";
-					} else if (versionInt <= VersionEnums.VERSION_115.getVersionInt() && entity.toString().equals("PIG_ZOMBIE")) { // 1.15 and below (must use entity name due to unknown field)
-						path = "pigman-messages";
-					} else if (versionInt >= VersionEnums.VERSION_19.getVersionInt() && entity == EntityType.SHULKER) { // 1.9
-						path = "shulker-messages";
-					} else if (versionInt >= VersionEnums.VERSION_110.getVersionInt() && entity == EntityType.STRAY) { // 1.10
-						path = "stray-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.LLAMA) { // 1.11
-						path = "llama-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.DRAGON_FIREBALL) { // id changed in 1.11
-						path = "dragon-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.LLAMA_SPIT) { // 1.11
-						path = "llama-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.WITHER_SKELETON) { // differation of wither skeletons and skeletons added 1.11
-						path = "witherskeleton-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.ELDER_GUARDIAN) { // differation of elder guardians and guardians added 1.11
-						path = "elderguardian-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.ZOMBIE_VILLAGER) { // differation of zombies and zombie villagers added 1.11
-						path = "zombievillager-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.EVOKER) { // 1.11
-						path = "evoker-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.VEX) { // 1.11
-						path = "vex-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.VINDICATOR) { // 1.11
-						path = "vindicator-messages";
-					} else if (versionInt >= VersionEnums.VERSION_111.getVersionInt() && entity == EntityType.HUSK) { // 1.11
-						path = "husk-messages";
-					} else if (versionInt >= VersionEnums.VERSION_112.getVersionInt() && entity == EntityType.ILLUSIONER) { // 1.12
-						path = "illusioner-messages";
-					} else if (versionInt >= VersionEnums.VERSION_113.getVersionInt() && entity == EntityType.PHANTOM) { // 1.13
-						path = "phantom-messages";
-					} else if (versionInt >= VersionEnums.VERSION_113.getVersionInt() && entity == EntityType.DROWNED) { // 1.13
-						path = "drowned-messages";
-					} else if (versionInt >= VersionEnums.VERSION_113.getVersionInt() && entity == EntityType.PUFFERFISH) { // 1.13
-						path = "pufferfish-messages";
-					} else if (versionInt >= VersionEnums.VERSION_113.getVersionInt() && entity == EntityType.POLAR_BEAR) { // 1.13
-						path = "polar-bear-messages";
-					} else if (versionInt >= VersionEnums.VERSION_113.getVersionInt() && entity == EntityType.DOLPHIN) { // 1.13
-						path = "dolphin-messages";
-					} else if (versionInt >= VersionEnums.VERSION_114.getVersionInt() && entity == EntityType.PILLAGER) { // 1.14
-						path = "pillager-messages";
-					} else if (versionInt >= VersionEnums.VERSION_114.getVersionInt() && entity == EntityType.RAVAGER) { // 1.14
-						path = "ravager-messages";
-					} else if (versionInt >= VersionEnums.VERSION_114.getVersionInt() && entity == EntityType.TRADER_LLAMA) { // 1.14
-						path = "llama-messages";
-					} else if (versionInt >= VersionEnums.VERSION_114.getVersionInt() && entity == EntityType.PANDA) { // 1.14
-						path = "panda-messages";
-					} else if (versionInt >= VersionEnums.VERSION_115.getVersionInt() && entity == EntityType.BEE) { // 1.15
-						path = "bee-messages";
-					} else if (versionInt >= VersionEnums.VERSION_116.getVersionInt() && entity == EntityType.PIGLIN) { // 1.16
-						path = "piglin-messages";
-					} else if (versionInt >= VersionEnums.VERSION_116.getVersionInt() && entity == EntityType.ZOGLIN) { // 1.16
-						path = "zoglin-messages";
-					} else if (versionInt >= VersionEnums.VERSION_116.getVersionInt() && entity == EntityType.HOGLIN) { // 1.16
-						path = "hoglin-messages";
-					} else if (versionInt >= VersionEnums.VERSION_116.getVersionInt() && entity == EntityType.ZOMBIFIED_PIGLIN) { // 1.16
-						path = "zombified-piglin-messages";
-					} else if (versionInt >= VersionEnums.VERSION_117.getVersionInt() && entity == EntityType.GOAT) { // 1.17
-						path = "goat-messages";
-					} else if (hasCustomName && plugin.getConfig().getBoolean("enable-custom-name-entity-messages")) {
+							break;
+
+						case "RAVAGER": // Since 1.14
+							path = "ravenger-messages";
+							break;
+
+						case "PANDA": // Since 1.14
+							path = "panda-messages";
+							break;
+
+						case "BEE": // Since 1.15
+							path = "bee-messages";
+							break;
+
+						case "PIGLIN": // Since 1.16
+							path = "piglin-messages";
+							break;
+
+						case "ZOGLIN": // Since 1.16
+							path = "zoglin-messages";
+							break;
+
+						case "HOGLIN": // Since 1.16
+							path = "hoglin-messages";
+							break;
+
+						case "ZOMBIFIED_PIGLIN": // Since 1.16
+							path = "zombified-piglin-messages";
+							break;
+
+						case "GOAT": // Since 1.17
+							path = "goat-messages";
+							break;
+
+						default: // Most likely not implemented. This serves as a placeholder message
+							path = "not-implemented-messages";
+
+					}
+
+					if (hasCustomName && plugin.getConfig().getBoolean("enable-custom-name-entity-messages")) {
 						path = "custom-name-entity-messages"; // Triggers for custom entities. Would recommend setting to true so that the not implemented setting wont be triggered.
 					}
 
